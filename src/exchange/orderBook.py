f03c7c711045a6933e127cd738e14be92a3c5ba8
Freddie Moralee <moralee-f-20@kcs.org.uk>
2026-07-09 17:00:03 +0100

    Implement order insertion and removal to manage books.
    Add methods to add new orders and to remove specific orders ensuring the maintenance of the books price levels.

--- src/exchange/orderBook.py
+++ src/exchange/orderBook.py
@@ -8,4 +8,21 @@ class orderBook:
         self.bids = defaultdict(deque)
         self.asks = defaultdict(deque)
 
-
+    def addOrder(self, order):
+        """Add orders to the correct dictionary dependant on their type. Stored in the dictionary at their price point."""
+        if order.side == 'buy':
+            self.bids[order.price].append(order)
+        else:
+            self.asks[order.price].append(order)
+        
+    def removeOrder(self, order):
+        """Remove the specific order, as passed in, from the matching dictionary."""
+        if order.side == 'buy':
+            self.bids[order.price].remove(order)
+            #if the last order at a given price level is removed ensure the entire price level is deleted
+            if not self.bids[order.price]:
+                del self.bids[order.price]
+        else:
+            self.asks[order.price].remove(order)
+            if not self.asks[order.price]:
+                del self.asks[order.price]
